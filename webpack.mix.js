b4f48304a6bce7ed1a9a83c54d792f824c2b9a5a
Abir D <61226387+Abir-Das@users.noreply.github.com>
2020-06-09 11:33:06 +0530

    design modified

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('src/js/app.js', 'dist/js')
-.sass('src/scss/app.scss', 'dist/css')
-.setPublicPath('dist');
+	.sass('src/scss/app.scss', 'dist/css')
+	.setResourceRoot('../')
+	.setPublicPath('dist');
 
